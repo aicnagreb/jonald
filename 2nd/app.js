c57730c8cf520ae2011788827222dd85b5aa92cb
aicnagreb <dlanojaicnagreb@gmail.com>
2023-03-09 14:06:50 +0800

    hahha

--- 2nd/app.js
+++ 2nd/app.js
@@ -3,17 +3,36 @@ data(){
 
     return{
 Lastname: 'Bergancia',
+LastnameA: 'Ares',
+LastnameP: 'Ares',
 firstname: 'jonald',
+firstnameA: 'Albert',
+firstnameP: 'John Kenneth',
 age: 22
 
     }
 },
 methods:{
     changeTitle(){
-        this.Lastname = 'gg'
-        this.Lastname = 'kk'
+        this.Lastname = 'Ares'
+        
+    },
+    changeName(){
+        
+        this.firstnameA = 'albert'
+
+    },
+    changek(){
+        this.LastnameP = 'Pastrana'
+        
+    },
+    changej(){
+        
+        this.firstname = 'John Kenneth'
+
     }
-}
+},
+
 
 
 })
